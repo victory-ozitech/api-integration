eba4c823c5b15ac45258e9a9c1b4be420de52a05
Wealth Ozitech <wealth.ozitech@gmail.com>
2026-05-26 13:35:14 +0100

    updates

---
+++ app/Http/Controllers/User/DashBoardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashBoardController extends Controller
+{
+    public function index()
+    {
+        // return inertia('Dashboard');
+    }
+}
