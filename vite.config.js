3bc9dda0cdae56a8c8e9a554fa7afd22b158c510
Victory Okon <victoryozitech1@gmail.com>
2026-05-26 14:04:51 +0100

    updates -- victory

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
         vue(),
     ],
+
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources"),
+        },
+    },
+
     server: {
         watch: {
             ignored: ["**/storage/framework/views/**"],
